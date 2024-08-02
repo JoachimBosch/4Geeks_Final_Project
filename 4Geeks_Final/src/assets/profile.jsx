299392b040b9775102af65d27b794179ef081d1f
Joachim Bosch <164736830+JoachimBosch@users.noreply.github.com>
2024-08-02 20:39:13 +0200

    Added header

--- 4Geeks_Final/src/assets/profile.jsx
+++ 4Geeks_Final/src/assets/profile.jsx
@@ -3,8 +3,20 @@ import MyContext from "../Context/context";
 const Profile = () => {
     return (
         <>
-            <div>
-                This is my profile page
+            <div className="mt-20 flex flex-col">
+                <div>
+                    <h2>Personal information</h2>
+                    <div>
+                        <h3>Name:</h3>
+                        <input />
+                        <h3>Last name:</h3>
+                        <input />
+                        <h3>Email address:</h3>
+                        <input />
+                        <h3>Phone number:</h3>
+                        <input />
+                    </div>
+                </div>
             </div>
         </>
         )
